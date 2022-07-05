3c7dfe676852431d922050b76d9e2945582239e4
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-07-05 12:21:22 -0300

    CidadeServiceImpl criado

---
+++ src/main/java/br/univille/dacs2022/service/impl/CidadeServiceImpl.java
@@ -0,0 +1,29 @@
+package br.univille.dacs2022.service.impl;
+
+import java.util.List;
+
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.univille.dacs2022.dto.CidadeDTO;
+import br.univille.dacs2022.entity.Cidade;
+import br.univille.dacs2022.mapper.CidadeMapper;
+import br.univille.dacs2022.repository.CidadeRepository;
+import br.univille.dacs2022.service.CidadeService;
+
+@Service
+public class CidadeServiceImpl
+        implements CidadeService {
+
+    @Autowired
+    private CidadeRepository repository;
+
+    private CidadeMapper mapper = Mappers.getMapper(CidadeMapper.class);
+
+    @Override
+    public List<CidadeDTO> getAll() {
+        List<Cidade> listaCidades = repository.findAll();
+        return mapper.mapListCidade(listaCidades);
+    }
+
+}
