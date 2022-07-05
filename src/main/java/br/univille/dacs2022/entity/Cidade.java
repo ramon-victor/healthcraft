e76f8b7b21504b93aab43297cd187d793cc9124c
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-07-05 11:16:35 -0300

    Criada a classe Cidade e sua associação com Paciente

---
+++ src/main/java/br/univille/dacs2022/entity/Cidade.java
@@ -0,0 +1,29 @@
+package br.univille.dacs2022.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(length = 100)
+    private String nome;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
--- src/main/java/br/univille/dacs2022/entity/Paciente.java
+++ src/main/java/br/univille/dacs2022/entity/Paciente.java
@@ -2,13 +2,17 @@ package br.univille.dacs2022.entity;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.ManyToAny;
 
 // POJO - Plain Old Java Object
 @Entity
@@ -21,6 +25,8 @@ public class Paciente {
     private String sexo;
     @Temporal(value = TemporalType.DATE)
     private Date dataNascimento;
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH})
+    private Cidade cidade;
 
     public long getId() {
         return id;
