9b7fd269ef8d3738a1a9afc2e0be966bd5746ff4
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-07-05 11:57:12 -0300

    CidadeMapper criado

---
+++ src/main/java/br/univille/dacs2022/mapper/CidadeMapper.java
@@ -0,0 +1,16 @@
+package br.univille.dacs2022.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import br.univille.dacs2022.dto.CidadeDTO;
+import br.univille.dacs2022.entity.Cidade;
+
+@Mapper
+public interface CidadeMapper {
+    List<CidadeDTO> mapListCidade(List<Cidade> cidade);
+    List<Cidade> mapListCidadeDTO(List<CidadeDTO> cidade);
+
+    CidadeDTO mapCidade(Cidade cidade);
+    Cidade mapCidadeDTO(CidadeDTO cidade);
+}
