6ce97b60742b704e27da63aa64780ac15916f2d9
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-07-05 11:31:41 -0300

    Criado a classe DTO da Cidade

---
+++ src/main/java/br/univille/dacs2022/dto/CIdadeDTO.java
@@ -0,0 +1,19 @@
+package br.univille.dacs2022.dto;
+
+public class CIdadeDTO {
+    private long id;
+    private String nome;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
