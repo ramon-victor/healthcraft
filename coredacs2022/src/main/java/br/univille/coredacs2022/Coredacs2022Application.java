17a3089ae6fa25a607e165dd408b37651f86e986
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-08-03 20:12:03 -0300

    Dependência coredacs2022 criada

---
+++ coredacs2022/src/main/java/br/univille/coredacs2022/Coredacs2022Application.java
@@ -0,0 +1,13 @@
+package br.univille.coredacs2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coredacs2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coredacs2022Application.class, args);
+	}
+
+}
---
+++ coredacs2022/src/test/java/br/univille/coredacs2022/Coredacs2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.coredacs2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coredacs2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
