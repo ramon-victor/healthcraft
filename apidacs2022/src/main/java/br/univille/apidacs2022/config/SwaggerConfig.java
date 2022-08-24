5e879dd05a3c24f9310598d8e070255fce034574
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-08-24 20:03:57 -0300

    Documentação API

--- apidacs2022/src/main/java/br/univille/apidacs2022/config/SwaggerConfig.java
+++ apidacs2022/src/main/java/br/univille/apidacs2022/config/SwaggerConfig.java
@@ -1,9 +1,14 @@
 package br.univille.apidacs2022.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -18,5 +23,15 @@ public class SwaggerConfig {
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .version("v1")
                 .build();
+    }
+
+    @Bean
+    public Docket api2() {
+        return new Docket(DocumentationType.OAS_30)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage(""))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiInfo());
     }
 }
