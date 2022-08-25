28e23e8e3cfb3746f055b12f7a8acadd5413f548
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-08-24 21:57:09 -0300

    Adicionado teste unitarios

--- apidacs2022/src/test/java/br/univille/apidacs2022/Apidacs2022ApplicationTests.java
+++ apidacs2022/src/test/java/br/univille/apidacs2022/Apidacs2022ApplicationTests.java
@@ -1,13 +1,54 @@
 package br.univille.apidacs2022;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import br.univille.apidacs2022.api.PacienteControllerAPI;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Apidacs2022ApplicationTests {
+
+	@Autowired
+	private PacienteControllerAPI pacienteControllerAPI;
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertThat(pacienteControllerAPI).isNotNull();
+	}
+
+	@Test
+	void pacienteControllerAPIPOSTGETTest() throws Exception{
+		MvcResult result = 
+		mockMvc.perform(post("/api/v1/pacientes")
+		.content("{\"nome\":\"Zezinho\",\"sexo\":\"Masculino\"}")
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isCreated()).andReturn();
+
+		String resultString = result.getResponse().getContentAsString();
+		JSONObject objJson = new JSONObject(resultString);
+		
+		mockMvc.perform(get("/api/v1/pacientes/" + objJson.getString("id")))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.nome", is("Zezinho")))
+			.andExpect(jsonPath("$.sexo", is("Masculino")));
 	}
 
 }
