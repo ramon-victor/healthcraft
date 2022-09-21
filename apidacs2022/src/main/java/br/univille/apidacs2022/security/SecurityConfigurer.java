8150293231c95f0da39c7fb0fd63e169b35e9c02
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-09-21 20:46:07 -0300

    Implementado autorização JWT para API

--- apidacs2022/src/main/java/br/univille/apidacs2022/security/SecurityConfigurer.java
+++ apidacs2022/src/main/java/br/univille/apidacs2022/security/SecurityConfigurer.java
@@ -10,10 +10,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
-    /*Usar este codigo enquanto nao concluir JWT */
+    @Autowired
+    private JWTRequestFilter jwtRequestFilter;
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .authorizeRequests().antMatchers("/swagger-ui.html","/v2/api-docs/**","/swagger-resources/**","/api/**").permitAll();
+            .authorizeRequests()
+            .antMatchers("/api/v1/auth/signin", "/swagger-ui.html","/v2/api-docs/**","/swagger-resources/**")
+            .permitAll()
+            .antMatchers("/api/**").authenticated()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
