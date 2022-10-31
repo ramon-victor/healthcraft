c0ada08eceb190b012e0d7703fb0b07a6d6e1573
RAMON VICTOR CARDOSO <ramon.cardoso@univille.br>
2022-10-31 11:50:41 -0300

    Configurações Docker

--- apidacs2022/src/main/java/br/univille/apidacs2022/security/JWTRequestFilter.java
+++ apidacs2022/src/main/java/br/univille/apidacs2022/security/JWTRequestFilter.java
@@ -12,49 +12,46 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-public class JWTRequestFilter extends OncePerRequestFilter{
-
+public class JWTRequestFilter extends OncePerRequestFilter {
 
     @Autowired
     private JWTUtil serviceJWT;
     @Autowired
     private UserDetailsService userService;
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
-            String authorizationHeader = request.getHeader("Authorization");
-            String username = null; 
-            String token = null;
 
-            if(authorizationHeader != null &&
-            authorizationHeader.startsWith("Bearer ")) {
-                token = authorizationHeader.substring(7);
-                username = serviceJWT.extractUserName(token);
-            }
+        String authorizationHeader = request.getHeader("Authorization");
+        String username = null;
+        String token = null;
 
-            if(username != null &&
+        if (authorizationHeader != null &&
+                authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7);
+            username = serviceJWT.extractUserName(token);
+        }
+
+        if (username != null &&
                 SecurityContextHolder.getContext().getAuthentication() == null) {
-                    UserDetails userDetails = userService.loadUserByUsername(username);
-                if(serviceJWT.validateToken(token, userDetails)) {
-                    UsernamePasswordAuthenticationToken ut = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            UserDetails userDetails = userService.loadUserByUsername(username);
+            if (serviceJWT.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken ut = new UsernamePasswordAuthenticationToken(userDetails, null,
+                        userDetails.getAuthorities());
 
-                    ut.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(ut);
-                }
+                ut.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(ut);
             }
+        }
 
-
-            filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response);
 
     }
-    
+
 }
